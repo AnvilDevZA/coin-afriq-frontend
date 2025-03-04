64c2a146c29a508e297500a7016f4293bc5ce21b
AnvilDevZa <websmith@anvil-dev.site>
2025-03-04 16:30:07 +0200

    add new links

--- menu.config.ts
+++ menu.config.ts
@@ -3,8 +3,9 @@ export const mainMenu = {
   home: "/",
   about: "/pages/what-exactly-is-coinafriq",
   "getting started": "/pages/getting-started",
-  "for individuals": "/pages/coin-afriq-for-individuals",
+  "for individuals": "/pages/coinafriq-for-individuals",
   "for businesses": "/pages/coin-afriq-for-business",
+  whitepaper: "/pages/coin-afriq-whitepaper/",
   blog: "/posts",
 };
 
