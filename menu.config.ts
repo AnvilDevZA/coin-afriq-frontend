d9bd029d94faeee691f29e5388552965d23640fb
AnvilDevZa <websmith@anvil-dev.site>
2025-03-10 15:21:17 +0200

    adding business plan

--- menu.config.ts
+++ menu.config.ts
@@ -7,6 +7,7 @@ export const mainMenu = {
   "for individuals": "/pages/coinafriq-for-individuals",
   "for businesses": "/pages/coin-afriq-for-business",
   whitepaper: "/pages/coin-afriq-whitepaper/",
+  "Business Plan": "/pages/business-plan",
   blog: "/posts",
 };
 
