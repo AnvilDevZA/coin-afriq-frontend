4420a4d3750c51aeef76061eb0a57a567359f39f
AnvilDevZA <websmith@anvil-site.dev>
2025-06-24 13:30:09 +0200

    fix social link

--- components/footer.tsx
+++ components/footer.tsx
@@ -55,7 +55,7 @@ export default function Footer( props: any ) {
             <rect width={20} height={20} x={2} y={2} rx={5} ry={5} />
             <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37zM17.5 6.5h.01" />
           </svg></Link></li>
-          <li><Link href="https://x.com/CoinAfriq"><svg
+          <li><Link href="https://x.com/CoinAfriqZA"><svg
             xmlns="http://www.w3.org/2000/svg"
             width={24}
             height={24}
